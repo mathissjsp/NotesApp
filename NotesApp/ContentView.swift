26b04702f60514ab26d4fa905e06f5433428caf8
mathissjsp <mathis.goffin@jopie.be>
2025-01-23 11:28:33 +0100

    Initial Commit

---
+++ NotesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesApp
+//
+//  Created by mathis goffin on 23/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by mathis goffin on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
