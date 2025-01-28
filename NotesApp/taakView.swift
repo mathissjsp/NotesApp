b78e2e61025ee7c7f9defeca30c43e327695d2da
mathissjsp <mathis.goffin@jopie.be>
2025-01-28 14:03:40 +0100

    update 1.2

---
+++ NotesApp/taakView.swift
@@ -0,0 +1,18 @@
+//
+//  taakView.swift
+//  NotesApp
+//
+//  Created by mathis goffin on 28/01/2025.
+//
+
+import SwiftUI
+
+struct taakView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    taakView()
+}
