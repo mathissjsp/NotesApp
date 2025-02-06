6834cbab55a09370284cf91950af3e59459b0747
mathissjsp <mathis.goffin@jopie.be>
2025-02-06 11:57:49 +0100

    klaar

--- NotesApp/ContentView.swift
+++ NotesApp/ContentView.swift
@@ -7,38 +7,69 @@
 
 import SwiftUI
 
+struct Taak: Identifiable {
+    let id = UUID()
+    var completed: Bool
+    var naam: String
+}
+
 
 class TakenModel: ObservableObject {
-    @Published  var taken: [String]  = ["Test taak"]
+    @Published  var taken: [Taak]  = [Taak(completed: true, naam: "test")]
     
     
-    func voegTaakToe(title: String) {
-        taken.append(title)
+    func voegTaakToe(title: String, completed: Bool) {
+        taken.append(Taak(completed: completed, naam: title))
     }
     
     func deleteItem(at offsets: IndexSet) {
         taken.remove(atOffsets: offsets)
+        
+        
+        }
+    func bewerkTaak(id: UUID, nieuweNaam: String, nieuweStatus: Bool) {
+        if let index = taken.firstIndex(where: { $0.id == id }) {
+            taken[index].naam = nieuweNaam
+            taken[index].completed = nieuweStatus
+        }
     }
+    
+    
+    
 }
+
 struct ContentView: View {
     @StateObject var model = TakenModel()
     @State var taak: String = ""
+    @State var taakCompleted: Bool = false
     
-   
+    
+    
     
     var body: some View {
         NavigationStack{
-           
+            
             VStack{
                 List{
-                    ForEach(model.taken, id: \.self) { taak in
-                        NavigationLink("\(taak)", destination: taakView())
+                    ForEach(model.taken) { taak in
+                        NavigationLink(destination: taakView(Taak:taak, model: model)){
+                            HStack {
+                                Text("\(taak.naam)")
+                                Spacer()
+                                Image(systemName: taak.completed ? "checkmark.square.fill" : "x.square.fill").foregroundColor(taak.completed ? .green : .red)
+                            }
+                        }
                         
                     }.onDelete(perform: model.deleteItem)
+                    
+                    
                 }
                 TextField("voeg je taak toe", text: $taak).padding().background(.white).padding()
+                Toggle(isOn: $taakCompleted) {
+                    Text("klaar?")
+                }.padding()
                 Button(action: {
-                    model.voegTaakToe(title: taak)
+                    model.voegTaakToe(title: taak, completed: taakCompleted)
                     taak = ""
                 }) {
                     Text("Taak toevoegen").padding().padding(.horizontal, 100).background(.gray).cornerRadius(15).foregroundColor(.white)
@@ -47,7 +78,7 @@ struct ContentView: View {
             .navigationTitle("taken")
             .background(Color(.systemGray6))
         }
-    
+        
     }
 }
 
--- NotesApp/taakView.swift
+++ NotesApp/taakView.swift
@@ -1,19 +1,49 @@
 //
 //  taakView.swift
 //  NotesApp
-//
+//  
 //  Created by mathis goffin on 28/01/2025.
 //
 
 import SwiftUI
 
 struct taakView: View {
-     
+     var Taak: Taak
+    //model binnenhalen
+    @ObservedObject var model: TakenModel
+    //niewe state voor naam
+    @State var nieweNaam = ""
+    
+    @State var nieweStatus = false
+    
+    //functie find index
+    
     var body: some View {
-        Text("hello world")
+        VStack {
+                   Text(Taak.naam)
+                       .font(.largeTitle)
+                       .padding()
+                   
+                   // Hier kun je nog meer informatie tonen, zoals de voltooiingsstatus van de taak
+                   Text(Taak.completed ? "Voltooid" : "Nog niet voltooid")
+                       .padding()
+                       .foregroundColor(Taak.completed ? .green : .red)
+            TextField("", text: $nieweNaam).textFieldStyle(.roundedBorder)
+            Toggle("Taak voltooid?", isOn: $nieweStatus)
+            
+            Button(action: {
+                model.bewerkTaak(id: Taak.id , nieuweNaam: nieweNaam, nieuweStatus: nieweStatus)
+            }){
+                Text("opslaan")
+            }
+            .onAppear {
+                        nieweNaam = Taak.naam
+                        nieweStatus = Taak.completed
+                    }
+            
+            
+            
+        }.padding()
+               .navigationTitle(Taak.naam)
     }
-}
-
-#Preview {
-    taakView()
 }
